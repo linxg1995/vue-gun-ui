3b4d161740a220dcc2a48320c25d6eb4c7791bc3
linxiaogang <linxiaogang@badousoft.com>
2020-06-08 15:22:21 +0800

    工具类函数，quicksort()，快速排序；

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -4,6 +4,38 @@
  * @Date: 2020-05-14
  * @LastEditTime: 2020-06-08
  */
+/**
+     * @description: 数组快速排序
+     * @param {Array} sourceArr 源数组
+     * @param {String} conf 配置
+     */
+function quicksort(sourceArr, conf = {}) {
+    let config = Object.assign({
+        sortKey: '', // 排序字段
+        DESC: false // 降序
+    }, conf)
+
+    if (sourceArr.length <= 1) {
+        return sourceArr
+    }
+    const basic = sourceArr[0]
+    let left = [], right = []
+    for (let i = 1; i < sourceArr.length; i++) {
+        if ((sourceArr[i][config.sortKey] || sourceArr[i])
+            <
+            (basic[config.sortKey] || basic)) {
+            left.push(sourceArr[i])
+        } else {
+            right.push(sourceArr[i])
+        }
+    }
+    if (config.DESC) {
+        return [...quicksort(right, conf), basic, ...quicksort(left, conf)]
+    }
+    return [...quicksort(left, conf), basic, ...quicksort(right, conf)]
+
+}
+
 export default {
     /**
      * @description: 校验非空
@@ -79,10 +111,11 @@ export default {
      * @param {Array} propQueue 属性队列
      */
     optChain(sourceObj, propQueue = []) {
-        let val = sourceObj || undefined
+        let val = sourceObj
         for (let i = 0; i < propQueue.length; i++) {
             val = val?.[propQueue[i]]
         }
         return val
-    }
+    },
+    quicksort
 }
